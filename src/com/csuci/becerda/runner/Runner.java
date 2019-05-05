52a60d179dcccf70b0ba9d2bab4dd510dda7ee9e
becerda <benjamin.cerda773@myci.csuci.edu>
2019-05-04 20:06:31 -0700

    Removed some TODO comments

--- src/com/csuci/becerda/runner/Runner.java
+++ src/com/csuci/becerda/runner/Runner.java
@@ -17,16 +17,12 @@ public class Runner {
 		try {
 			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (UnsupportedLookAndFeelException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
 			new MainWindow();
